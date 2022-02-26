a7b844d4e36bfd07591cec162d549b1466d2f53e
Emily3403 <seebeckemily3403@gmail.com>
2022-02-26 23:51:48 +0100

    no message.

--- roles/gui_basic/tasks/dotfiles/chromium/Default/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm/1.41.4_0/js/document-blocked.js
+++ roles/gui_basic/tasks/dotfiles/chromium/Default/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm/1.41.4_0/js/document-blocked.js
@@ -1,236 +1,256 @@
-#!/usr/bin/env bash
-#Author: Daniel Elf
-#Tested w/ btrfs-progs v4.14.1
-#Description: Somewhat interactive "undeleter" for BTRFS file systems.
-#  This will not work for every file in every scenario
-#  The best 'undeletion' you can do is to recover from backup :-)
-#Syntax: ./undeletebtrfs.sh <dev> <dst>
-#Example: ./undeletebtrfs.sh /dev/sda1 /mnt/undeleted
-#NOTE: device must be unmounted
-# var declarations
-dev=$1
-dst=$2
-roots="/tmp/btrfsroots.tmp"
-depth=0
-tmp="/tmp/undeleter.tmp"
-IFS=$'\n'
-rectype="none"
-# vars that can be used to change font color
-blue=$(tput setaf 6)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-red=$(tput setaf 1)
-normal=$(tput sgr0) # default color
+/*******************************************************************************
 
+    uBlock Origin - a browser extension to block requests.
+    Copyright (C) 2015-present Raymond Hill
 
-# Functions
-function titleprint(){
-  printf "========================\n| ${yellow}BTRFS File Undeleter${normal} |\n========================\n"
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+    Home: https://github.com/gorhill/uBlock
+*/
+
+/* global uDom */
+
+'use strict';
+
+/******************************************************************************/
+
+(( ) => {
+
+/******************************************************************************/
+
+const messaging = vAPI.messaging;
+let details = {};
+
+{
+    const matches = /details=([^&]+)/.exec(window.location.search);
+    if ( matches !== null ) {
+        details = JSON.parse(decodeURIComponent(matches[1]));
+    }
 }
 
-function spinner(){
-  # This function takes care of the spinner used for long-lasting tasks
-    local pid=$!
-    local delay=0.75
-    local spinstr='|/-'\\
-    while [ -d /proc/"$pid" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
+/******************************************************************************/
+
+(async ( ) => {
+    const response = await messaging.send('documentBlocked', {
+        what: 'listsFromNetFilter',
+        rawFilter: details.fs,
+    });
+    if ( response instanceof Object === false ) { return; }
+
+    let lists;
+    for ( const rawFilter in response ) {
+        if ( response.hasOwnProperty(rawFilter) ) {
+            lists = response[rawFilter];
+            break;
+        }
+    }
+
+    if ( Array.isArray(lists) === false || lists.length === 0 ) { return; }
+
+    const parent = uDom.nodeFromSelector('#whyex > span:nth-of-type(2)');
+    for ( const list of lists ) {
+        const listElem = document.querySelector('#templates .filterList')
+                                 .cloneNode(true);
+        const sourceElem = listElem.querySelector('.filterListSource');
+        sourceElem.href += encodeURIComponent(list.assetKey);
+        sourceElem.textContent = list.title;
+        if ( typeof list.supportURL === 'string' && list.supportURL !== '' ) {
+            const supportElem = listElem.querySelector('.filterListSupport');
+            supportElem.setAttribute('href', list.supportURL);
+            supportElem.classList.remove('hidden');
+        }
+        parent.appendChild(listElem);
+    }
+    uDom.nodeFromId('whyex').style.removeProperty('display');
+})();
+
+/******************************************************************************/
+
+(( ) => {
+    const matches = /^(.*)\{\{hostname\}\}(.*)$/.exec(vAPI.i18n('docblockedProceed'));
+    if ( matches === null ) { return; }
+    const proceed = document.querySelector('#templates .proceed').cloneNode(true);
+    proceed.children[0].textContent = matches[1];
+    proceed.children[2].textContent = matches[2];
+    const hnOption = proceed.querySelector('.hn');
+    if ( details.hn !== details.dn ) {
+        hnOption.textContent = details.hn;
+        hnOption.setAttribute('value', details.hn);
+    } else {
+        hnOption.remove();
+    }
+    const dnOption = proceed.querySelector('.dn');
+    dnOption.textContent = details.dn;
+    dnOption.setAttribute('value', details.dn);
+    document.getElementById('proceed').append(proceed);
+})();
+
+/******************************************************************************/
+
+uDom.nodeFromSelector('#theURL > p > span:first-of-type').textContent = details.url;
+uDom.nodeFromId('why').textContent = details.fs;
+
+/******************************************************************************/
+
+// https://github.com/gorhill/uBlock/issues/691
+//   Parse URL to extract as much useful information as possible. This is
+//   useful to assist the user in deciding whether to navigate to the web page.
+(( ) => {
+    if ( typeof URL !== 'function' ) { return; }
+
+    const reURL = /^https?:\/\//;
+
+    const liFromParam = function(name, value) {
+        if ( value === '' ) {
+            value = name;
+            name = '';
+        }
+        const li = document.createElement('li');
+        let span = document.createElement('span');
+        span.textContent = name;
+        li.appendChild(span);
+        if ( name !== '' && value !== '' ) {
+            li.appendChild(document.createTextNode(' = '));
+        }
+        span = document.createElement('span');
+        if ( reURL.test(value) ) {
+            const a = document.createElement('a');
+            a.href = a.textContent = value;
+            span.appendChild(a);
+        } else {
+            span.textContent = value;
+        }
+        li.appendChild(span);
+        return li;
+    };
+
+    // https://github.com/uBlockOrigin/uBlock-issues/issues/1649
+    //   Limit recursion.
+    const renderParams = function(parentNode, rawURL, depth = 0) {
+        let url;
+        try {
+            url = new URL(rawURL);
+        } catch(ex) {
+            return false;
+        }
+
+        const search = url.search.slice(1);
+        if ( search === '' ) { return false; }
+
+        url.search = '';
+        const li = liFromParam(vAPI.i18n('docblockedNoParamsPrompt'), url.href);
+        parentNode.appendChild(li);
+
+        const params = new self.URLSearchParams(search);
+        for ( const [ name, value ] of params ) {
+            const li = liFromParam(name, value);
+            if ( depth < 2 && reURL.test(value) ) {
+                const ul = document.createElement('ul');
+                renderParams(ul, value, depth + 1);
+                li.appendChild(ul);
+            }
+            parentNode.appendChild(li);
+        }
+
+        return true;
+    };
+
+    if ( renderParams(uDom.nodeFromId('parsed'), details.url) === false ) {
+        return;
+    }
+
+    const toggler = document.querySelector('#toggleParse');
+    toggler.classList.remove('hidden');
+
+    toggler.addEventListener('click', ( ) => {
+        const cl = uDom.nodeFromId('theURL').classList;
+        cl.toggle('collapsed');
+        vAPI.localStorage.setItem(
+            'document-blocked-expand-url',
+            (cl.contains('collapsed') === false).toString()
+        );
+    });
+
+    vAPI.localStorage.getItemAsync('document-blocked-expand-url').then(value => {
+        uDom.nodeFromId('theURL').classList.toggle(
+            'collapsed',
+            value !== 'true' && value !== true
+        );
+    });
+})();
+
+/******************************************************************************/
+
+// https://www.reddit.com/r/uBlockOrigin/comments/breeux/close_this_window_doesnt_work_on_firefox/
+
+if ( window.history.length > 1 ) {
+    uDom('#back').on(
+        'click',
+        ( ) => {
+            window.history.back();
+        }
+    );
+    uDom('#bye').css('display', 'none');
+} else {
+    uDom('#bye').on(
+        'click',
+        ( ) => {
+            messaging.send('documentBlocked', {
+                what: 'closeThisTab',
+            });
+        }
+    );
+    uDom('#back').css('display', 'none');
 }
 
-function syntaxcheck(){
-  # Check syntax
-  if [[ -z $dev || -z $dst ]]; then
-    titleprint
-    printf "${red}Error: ${yellow}Invalid syntax or missing required parameters\n"
-    printf "${normal}Syntax: ./script.sh ${blue}<dev> <dst>${normal}\n"
-    printf "${green}Example: ${normal}sudo ./undelete.sh ${blue}/dev/sda1 /mnt/${normal}\n\n"
-    exit 1
-  elif [[ $EUID -ne 0 ]]; then
-    titleprint
-    printf "${red}Error:${yellow} This script must be run with sudo (or as root) as btrfs restore requires it.\n"
-    printf "${normal}Syntax example: sudo ./undelete.sh ${blue}/dev/sda1 /mnt/${normal}\n"
-    printf "\n${yellow}Exiting...\n${normal}"
-    exit 1
-  fi
+/******************************************************************************/
 
-}
+const getTargetHostname = function() {
+    const elem = document.querySelector('#proceed select');
+    if ( elem === null ) { return details.hn; }
+    return elem.value;
+};
 
-function mountcheck(){
-  mount=$(grep -cw "$dev" /etc/mtab)
-  if [[ ! $mount == "0" ]]; then
-    titleprint
-    printf "${red}Error: ${blue}%s${yellow} is mounted! \nThis script can only be run against umounted devices. Please try again\n\n" "$dev"
-    printf "Exiting...\n${normal}"
-    exit 1
-  fi
-}
+const proceedToURL = function() {
+    window.location.replace(details.url);
+};
 
-function regexbuild(){
-  # Build regex for single share
-  >$tmp
-  printf "Below, enter the path to a file or folder, excluding the mountpoint\n"
-  printf "Example file: /data/documents/daniel.txt\n"
-  printf "How to write it: /documents/daniel.txt\n"
-  printf "Example directory: /data/pictures/important/\n"
-  printf "How to write it: /pictures/important/\n\n"
-  read -r -p "Enter path to file: " filepath
-  while [[ -z "$filepath" ]]; do
-    printf "\n${red}Err: No input given, try again.\n${normal}"
-    read -r -p "Enter path to file: " filepath
-  done
-  # Pick out the dir and filename
-  dirname=$(echo "$filepath" | awk -F"/" '{ print $(NF-1) }')
-  filename=$(echo "$filepath" | awk -F"/" '{ print $NF }')
-  # Check is first character is a /, if so ignore it
-  if [[ $filepath == /* ]]; then
-    filepath=$(echo "$filepath"| cut -c2-)
-  fi
-  # Determine type of recovery.. are we doing full folder or single file?
-  # Not used right now but maybe in a future version...
-  if [[ $filepath == */ ]]; then
-    rectype="folder"
-    recname="$dirname"
-    filepath+=".*"
-  else
-    rectype="file"
-    recname="$filename"
-  fi
-  # Read provided path to array
-  readarray -d/ -t filepatharray < <(echo "$filepath")
-  # Build the first set
-  regex="(|"${filepatharray[@]::1}""
-  # Build the rest, add them one by one
-  for i in "${filepatharray[@]:1}"; do
-    regex+=$(printf "(|/%s" "$i")
-  done
-  # Finally add enough ")" at the end
-  for i in "${filepatharray[@]}"; do
-    regex+=")"
-  done
-  printf "\nRegex:\n${blue}^/%s$ ${normal}\n\n" "$regex"
-  dryrun
-  checkresult
-}
+const proceedTemporary = async function() {
+    await messaging.send('documentBlocked', {
+        what: 'temporarilyWhitelistDocument',
+        hostname: getTargetHostname(),
+    });
+    proceedToURL();
+};
 
-function dryrun(){
-  # This is where we do the dryrun of BTRFS, this is used to quickly check if we can find the file using the provided regexbuild
-  # much faster than doing an actual restore.
-  if [[ $depth -eq 0 ]]; then
-    sudo btrfs restore -Div --path-regex '^/'${regex}'$' $dev /  2> /dev/null | grep -E "Restoring.*$recname" | cut -d" " -f 2- &> $tmp
-    # We have 3 levels: 0, 1 and 2. 0 means a basic 'btrfs restore', 1 and 2 means that we first get the roots and then loop them
-  elif [[ $depth -eq 1 ]]; then
-    while read -r i || [[ -n "$i" ]]; do
-      btrfs restore -t "$i" -Div --path-regex '^/'${regex}'$' "$dev" / 2> /dev/null | grep -E "Restoring.*$recname" | cut -d" " -f 2- &>> $tmp
-    done < "$roots"
-    # Level 2 is the 'deepest' level, here we add the -a flag to the btrfs-find-roots, this should give us way more roots to work with
-  elif [[ $depth -eq 2 ]]; then
-    while read -r i || [[ -n "$i" ]]; do
-      btrfs restore -t "$i" -Div --path-regex '^/'${regex}'$' "$dev" / 2> /dev/null| grep -E "Restoring.*$recname" | cut -d" " -f 2- &>> $tmp
-    done < "$roots"
-  fi
-}
+const proceedPermanent = async function() {
+    await messaging.send('documentBlocked', {
+        what: 'toggleHostnameSwitch',
+        name: 'no-strict-blocking',
+        hostname: getTargetHostname(),
+        deep: true,
+        state: true,
+        persist: true,
+    });
+    proceedToURL();
+};
 
-function checkresult(){
-  clear
-  titleprint
-  printf "Path entered: ${blue}%s${normal} \nRegex generated: ${blue}'^/%s\$'${normal} \nDepth-level: ${blue}%s${normal}\n\n" "$filepath" "$regex" "$depth"
-  if [[ ! -s $tmp && $depth -eq 0 ]]; then
-    # we didn't find any data on first attempt (as $tmp is empty)
-    depth=1
-    generateroots
-    dryrun
-    checkresult
-  elif [[ ! -s $tmp && $depth -eq 1 ]]; then
-    # didn't find any on the second attempt either
-    depth=2
-    generateroots
-    dryrun
-    checkresult
-  elif [[ -s $tmp ]]; then
-    # if $tmp is not empty, it means we found some data!
-      printf "${green}Data found!${normal} here are the file(s) found: \n====\n"
-      sort -u $tmp
-      printf "====\n\nChoose one of the following: \n${blue}1${normal}) Recover it \n${blue}2${normal}) Look one level deeper \n${blue}3${normal}) Try another path \n${blue}4${normal}) Exit\n"
-      while true; do
-        read -r -p "Enter choice: " input
-        case $input in
-          [1])
-            printf "\nRecover chosen...\n"
-            recover
-            ;;
-          [2])
-            if [[ $depth -eq 0 || $depth -eq 1 ]]; then
-              printf "\nTrying one level deeper...\n\n"
-              depth=$(($depth + 1))
-              generateroots
-              dryrun
-              checkresult
-            elif [[ $depth -eq 2 ]]; then
-              printf "You're already on the deepest level... Can't go deeper! \n\n"
-            fi
-            ;;
-          [3])
-            clear
-            printf "${yellow}Returning to path selection...${normal}\n\n"
-            depth=0
-            titleprint
-            regexbuild
-            ;;
-          [4])
-            exit 0
-            ;;
-          *)
-          printf "\nInvalid input.\n"
-        esac
-      done
-  else
-    printf "\n\n${red}Error:${normal} Unable to find the data, please verify the entered path and try again\n"
-    printf "Keep in mind that directory paths must end with a '/' \n\n"
-    read -s -p "Press Enter to return to start..."
-    clear
-    depth=0
-    printf "${yellow}Returning to path selection...${normal}\n\n"
-    titleprint
-    regexbuild
-  fi
-}
+uDom('#proceedTemporary').attr('href', details.url).on('click', proceedTemporary);
+uDom('#proceedPermanent').attr('href', details.url).on('click', proceedPermanent);
 
-function generateroots(){
-  if [[ $depth -eq 1 || $depth -eq 0 ]]; then
-    printf "Generating roots, please note that this may take a while to finish... "
-    btrfs-find-root "$dev" &> "$tmp"
-    grep -a Well "$tmp" | sed -r -e 's/Well block ([0-9]+).*/\1/' | sort -rn > "$roots"
-    printf "Done!\n"
-    rootcount=$(wc -l "$roots" | awk '{print $1}')
-    > "$tmp"
-    if [[ ! -s "$roots" ]]; then
-      printf "\n${yellow}Note:${normal} No (additional) roots found with btrfs-find-roots \nAttempting with -a flag (depth level 2)...\n"
-      depth=2
-      sleep 2
-      generateroots
-    fi
-  elif [[ $depth -eq 2 ]]; then
-    printf "Looking even deeper for roots, this can take quite a while... "
-    btrfs-find-root -a "$dev" &> "$tmp"
-    grep -a Well "$tmp" | sed -r -e 's/Well block ([0-9]+).*/\1/' | sort -rn > "$roots"
-    printf "Done!\n"
-    rootcount=$(wc -l $roots | awk '{print $1}')
-    > "$tmp"
-  fi
-}
+/******************************************************************************/
 
-function recover(){
-  # Attempt recovery of files
-  if [[ $depth = "0" ]]; then
-    sudo btrfs restore -iv --path-regex '^/'${regex}'$' "$dev" "$dst"  &> /dev/null
-    recoveredfiles=$(find $dst ! -empty -type f | wc -l)
-    # Find and delete empty recovered files, no point in keeping them around.
-    find $dst -empty -type f -delete
-  elif [[ $depth == "1" ]]; then
-    printf "Attempting
+})();
+
+/******************************************************************************/
--- roles/gui_basic/tasks/dotfiles/chromium/Default/Extensions/ijcpiojgefnkmcadacmacogglhjdjphj/1.0.30_0/help.js
+++
@@ -1,7 +0,0 @@
-var browser = browser || chrome;
-document.getElementById('cancelButton').addEventListener('click', () => {
-    browser.runtime.sendMessage({ acceptClicked: false, closeCallingTab: true }, () => { });
-});
-document.getElementById('acceptButton').addEventListener('click', () => {
-    browser.runtime.sendMessage({ acceptClicked: true, closeCallingTab: true }, () => { });
-});
